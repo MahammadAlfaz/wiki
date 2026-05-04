31dd9e8d1c853c0f1b6930dc3aadcfa337e4337c
mahammadalfaz.int <mahammadalfaz.int@niveussolutions.com>
2026-05-04 11:11:18 +0530

    feat: register query and documents routers into main app

--- main.py
+++ main.py
@@ -2,29 +2,33 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.api.routes import auth
+from app.api.routes import auth, documents, query
 from app.db.init_db import init_db
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
     yield
 
-app=FastAPI(
+
+app = FastAPI(
     title="Wiki -Devops knowledge Base",
     description="Rag chatbot for internal Devops documentation",
     version="1.0.0",
-    lifespan=lifespan
+    lifespan=lifespan,
 )
 
 app.include_router(auth.router)
+app.include_router(documents.router)
+app.include_router(query.router)
+
+
 @app.get("/")
 def root():
-    return {
-        'message':'Wiki API is running'
-    }
+    return {"message": "Wiki API is running"}
+
+
 @app.get("/health")
 def health():
-    return{
-        "status":'ok'
-    }
+    return {"status": "ok"}
